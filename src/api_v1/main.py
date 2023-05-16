99ea777b0c9c292d129fd08dec40ed7015f0a322
nsarubbi <nicosarubbi@gmail.com>
2023-05-16 19:38:55 -0300

    add stack

---
+++ src/api_v1/main.py
@@ -0,0 +1,4 @@
+
+def handler(event, context):
+    print("event: ", event)
+    return "hello world"
---
+++ src/settings/sandbox.py
@@ -0,0 +1,7 @@
+from os import getenv
+
+AWS_REGION = "us-east-1"
+AWS_ACCOUNT_ID = "174354456093"
+
+
+AWS_ACCOUNT_NAME = "nicosarubbi"
